6f8f24f0ce62fb9c895f0a71a67016fb15c9774a
Steve W <stevewicklund@gmail.com>
2019-09-18 12:09:06 -0400

    Update udpReceiver.py

--- udpReceiver.py
+++ udpReceiver.py
@@ -7,15 +7,12 @@ textport = sys.argv[1]
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = int(textport)
 server_address = ('localhost', port)
-s.bind(server_address)
 
 while True:
 
     print ("Waiting to receive on port %d : press Ctrl-C or Ctrl-Break to stop " % port)
 
     buf, address = s.recvfrom(port)
-    if not len(buf):
-        break
     print ("Received %s bytes from %s %s: " % (len(buf), address, buf ))
     s.sendto(buf.encode('utf-8'), server_address)
 
