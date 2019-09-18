e44e8e3c0a275c0a9187d56d0c6ffb16d75d5897
Steve W <stevewicklund@gmail.com>
2019-09-18 11:34:01 -0400

    Update UDPReceiver.java

--- UDPReceiver.java
+++ UDPReceiver.java
@@ -24,7 +24,7 @@ public class UDPReceiver {
 				socket.receive(packet);
 				System.out.println(packet.getAddress() + " " + packet.getPort() + ": " + new String(packet.getData()).trim());
 				
-				InetAddress host = InetAddress.getByName(new String(packet.getAddress()).trim());
+				InetAddress host = InetAddress.getByName((packet.getAddress()).toString());
 				String rMessage = new String(packet.getData()).trim();
 				String message = "ACK: " + rMessage;
 				byte[] data = message.getBytes();
