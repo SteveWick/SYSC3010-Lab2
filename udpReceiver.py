c33ec09e61c623ff1f3cf768a5f656717505fd03
Steve W <stevewicklund@gmail.com>
2019-09-18 12:32:35 -0400

    Update udpReceiver.py

--- udpReceiver.py
+++ udpReceiver.py
@@ -7,7 +7,7 @@ textport = sys.argv[1]
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = int(textport)
 server_address = ('localhost', port)
-
+s.bind(server_address)
 while True:
 
     print ("Waiting to receive on port %d : press Ctrl-C or Ctrl-Break to stop " % port)
